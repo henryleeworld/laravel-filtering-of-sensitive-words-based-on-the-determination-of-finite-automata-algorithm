7c18d85497c7dac0a87dda4dafd175d50a4d0331
Henry Lee <henryleeworld@gmail.com>
2020-09-11 20:01:02 +0800

    feat:升級至 laravel 8

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FilteringController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/filter', 'FilteringController@index');
+Route::get('/filter', [FilteringController::class, 'index']);
